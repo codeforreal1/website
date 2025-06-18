819f90fd0371468ac9e8359a1436fda8dbd6daf3
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-06-18 22:51:34 +0545

    feat: add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,40 +2,40 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import openGraphImages, { presets } from "astro-opengraph-images";
-import * as fs from "fs";
-import * as path from "path";
+// import openGraphImages, { presets } from "astro-opengraph-images";
+// import * as fs from "fs";
+// import * as path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import { renderOg } from "./src/utils/og-renderer";
+// import { renderOg } from "./src/utils/og-renderer";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default defineConfig({
   site: "https://codeforreal.com",
   integrations: [
     mdx(),
     sitemap(),
-    openGraphImages({
-      options: {
-        fonts: [
-          {
-            name: "Poppins",
-            weight: 400,
-            style: "normal",
-            data: fs.readFileSync(
-              path.join(
-                __dirname,
-                "node_modules/@fontsource/poppins/files/poppins-latin-400-normal.woff"
-              )
-            ),
-          },
-        ],
-      },
-      render: presets.blackAndWhite,
-    }),
+    // openGraphImages({
+    //   options: {
+    //     fonts: [
+    //       {
+    //         name: "Poppins",
+    //         weight: 400,
+    //         style: "normal",
+    //         data: fs.readFileSync(
+    //           path.join(
+    //             __dirname,
+    //             "node_modules/@fontsource/poppins/files/poppins-latin-400-normal.woff"
+    //           )
+    //         ),
+    //       },
+    //     ],
+    //   },
+    //   render: presets.blackAndWhite,
+    // }),
   ],
   vite: {
     plugins: [tailwindcss()],
