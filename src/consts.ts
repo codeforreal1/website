3646528440030cc3c22a0a7e1f62c0ebd165ef37
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-06-19 21:52:08 +0545

    feat: update seo description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Code For Real";
-export const SITE_DESCRIPTION = "Welcome to my world!";
+export const SITE_DESCRIPTION =
+  "Code For Real is a senior software engineer sharing real-world programming experiences across Web & Mobile Development, AI, UI/UX Design, DevOps, Data Structures & Algorithms, and Linux.";
