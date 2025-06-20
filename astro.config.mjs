2899221e2382105ca89ce1b934592cb086d8334e
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-06-20 21:34:15 +0545

    feat: update posthog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,42 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-// import openGraphImages, { presets } from "astro-opengraph-images";
-// import * as fs from "fs";
-// import * as path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-// import { renderOg } from "./src/utils/og-renderer";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: "/",
   site: "https://codeforreal.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    // openGraphImages({
-    //   options: {
-    //     fonts: [
-    //       {
-    //         name: "Poppins",
-    //         weight: 400,
-    //         style: "normal",
-    //         data: fs.readFileSync(
-    //           path.join(
-    //             __dirname,
-    //             "node_modules/@fontsource/poppins/files/poppins-latin-400-normal.woff"
-    //           )
-    //         ),
-    //       },
-    //     ],
-    //   },
-    //   render: presets.blackAndWhite,
-    // }),
-  ],
+  integrations: [mdx(), sitemap()],
   output: "static",
   vite: {
     plugins: [tailwindcss()],
